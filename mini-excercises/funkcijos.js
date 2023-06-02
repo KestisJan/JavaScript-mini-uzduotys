01f5a6e408dc2a8e7b8c2638af216f83e9ef8d0d
Kestas <kestutisjankevicius9@gmail.com>
2023-06-02 11:06:04 +0300

    Update funkcijos.js

--- mini-excercises/funkcijos.js
+++ mini-excercises/funkcijos.js
@@ -124,34 +124,10 @@ TESTAI:
 
 console.log(`Excercise - 4`);
 
-/* 
-priima vieną kintamąjį
-jei perduotas kintamasis nėra sąrašo tipo, tai išveda pranešimą “Pateikta netinkamo tipo reikšmė.”
-jei sąrašas yra tuščias, tai išveda pranešimą “Pateiktas sąrašas negali būti tuščias.”
-priešingu atveju, funkcija tęsia darbą
-pereina per visą pateiktą sąrašą ir į atskirą kintamąjį įsimena skaičių, kuris tuo metu yra didžiausias
-gražina didžiausią surastą skaičių
-TESTAI:
-    console.log( didziausiasSkaiciusSarase( [ 1 ] ) );
-        rezultatas: 1
-    console.log( didziausiasSkaiciusSarase( [ 1, 2, 3 ] ) );
-        rezultatas: 3
-    console.log( didziausiasSkaiciusSarase( [ -5, 78, 14, 0, 18 ] ) );
-        rezultatas: 78
-    console.log( didziausiasSkaiciusSarase( [ 69, 69, 69, 69, 66 ] ) );
-        rezultatas: 69
-    console.log( didziausiasSkaiciusSarase( [ -1, -2, -3, -4, -5, -6, -7, -8 ] ) );
-        rezultatas: -1
-    console.log( didziausiasSkaiciusSarase( “pomidoras” ) );
-        rezultatas: “Pateikta netinkamo tipo reikšmė.”
-    console.log( didziausiasSkaiciusSarase( [] ) );
-        rezultatas: “Pateiktas sąrašas negali būti tuščias.”
-*/
-
 function didziausiasSkaiciusSarase() {
     if (typeof list === object && list.length > 0) {
         return Math.max(list);
-    }
+    } 
 }
 
 
